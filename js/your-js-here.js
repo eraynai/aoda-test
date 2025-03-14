5745e9dfe7bd753ce3b409bf495fb3e5efb16d09
Elli Raynai <elli@multitouch.studio>
2025-03-14 10:43:46 -0400

    building form

--- js/your-js-here.js
+++ js/your-js-here.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+const menuButton = document.getElementById('menu-toggle');
+const primaryNav = document.getElementById('primary-nav');
+
+menuButton.addEventListener('click', function() {
+  const isExpanded = this.getAttribute('aria-expanded') === 'true';
+  
+  this.setAttribute('aria-expanded', String(!isExpanded));
+  primaryNav.hidden = isExpanded;
+});
